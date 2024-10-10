56c9515d7846b40b918334857c7a1dd72dfd9d90
Alexia Christelle <christellealexia53@gmail.com>
2024-10-10 14:57:50 +0200

    chore: add the providers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,25 +1,50 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
+  constructor(private readonly ninjasservice: NinjasService) {}
+
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('age') age: number) {
+    // const service = new NinjasService();
+    return this.ninjasservice.getNinjas(+age);
   }
+
   @Get(':id')
   getOneNinja(@Param('id') id: string) {
-    return id;
+    return this.ninjasservice.getOneNinja(+id);
   }
+
   @Put(':id')
-  updateNinja(@Param('id') id: string) {
-    return id;
+  updateNinja(@Param('id') id: string, @Body() updatedNinja: UpdateNinjaDto) {
+    return {
+      id,
+      name: updatedNinja.name,
+      age: updatedNinja.age,
+    };
   }
+
   @Delete(':id')
   deleteNinja(@Param('id') id: string) {
     return id;
   }
   @Post()
-  createNinja() {
-    return 'Ninja added';
+  createNinja(@Body() NinjaDto: CreateNinjaDto) {
+    return {
+      name: NinjaDto.name,
+      age: NinjaDto.age,
+    };
   }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+  private ninjas = [
+    { id: 0, name: 'Naruto', age: 18 },
+    { id: 1, name: 'Sasuke', age: 17 },
+    { id: 2, name: 'Itachi', age: 18 },
+    { id: 3, name: 'Minato', age: 17 },
+  ];
+
+  getNinjas(age?: number) {
+    if (age) {
+      return this.ninjas.filter((ninja) => ninja.age === age);
+    }
+    return this.ninjas;
+  }
+  getOneNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+    if (!ninja) throw new Error('Not found');
+    else return ninja;
+  }
+}
